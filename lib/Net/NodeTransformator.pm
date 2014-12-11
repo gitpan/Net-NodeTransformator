6fdb24d88ab672ca1ddadb3e9ca8ca9567972347
David Zurborg <post@david-zurb.org>
2014-12-11 13:50:01 +0000

    Import of ZURBORG/Net-NodeTransformator-0.104.tar.gz from CPAN.
    gitpan-cpan-distribution: Net-NodeTransformator
    gitpan-cpan-version:      0.104
    gitpan-cpan-path:         ZURBORG/Net-NodeTransformator-0.104.tar.gz
    gitpan-cpan-author:       ZURBORG
    gitpan-cpan-maturity:     released

--- lib/Net/NodeTransformator.pm
+++ lib/Net/NodeTransformator.pm
@@ -1,8 +1,10 @@
-package Net::NodeTransformator;
-# ABSTRACT: interface to node transformator
-
 use strict;
 use warnings;
+
+package Net::NodeTransformator;
+
+# ABSTRACT: interface to node transformator
+
 use AnyEvent;
 use AnyEvent::Handle;
 use AnyEvent::Socket;
@@ -11,168 +13,191 @@ use Try::Tiny;
 use Env::Path;
 use File::Temp qw(tempdir);
 use POSIX qw(getcwd);
+use CBOR::XS ();
+use Carp;
 
-our $VERSION = '0.103'; # VERSION
-
+our $VERSION = '0.104';    # VERSION
 
 sub new {
-	my ($class, $hostport) = @_;
-	if ($hostport !~ m{:}) {
-		if ($hostport =~ m{^\d+$}) {
-			$hostport = "localhost:$hostport";
-		} else {
-			$hostport = "unix/:$hostport";
-		}
-	}
-	my ($host, $port) = parse_hostport($hostport);
-	if ($host eq 'unix/' and $port !~ m{^/}) {
-		$port = getcwd.'/'.$port;
-	}
-	bless {
-		host => $host,
-		port => $port,
-	} => ref $class || $class;
+    my ( $class, $hostport ) = @_;
+    if ( $hostport !~ m{:} ) {
+        if ( $hostport =~ m{^\d+$} ) {
+            $hostport = "localhost:$hostport";
+        }
+        else {
+            $hostport = "unix/:$hostport";
+        }
+    }
+    my ( $host, $port ) = parse_hostport($hostport);
+    if ( $host eq 'unix/' and $port !~ m{^/} ) {
+        $port = getcwd . '/' . $port;
+    }
+    bless {
+        host => $host,
+        port => $port,
+      } => ref $class
+      || $class;
 }
-
 
 sub standalone {
-	my ($class, $hostport) = @_;
+    my $class = shift;
 
-	my $bin = 'transformator';
-	my ($path) = Env::Path->PATH->Whence($bin);
+    my %options;
+    if ( @_ > 1 and !( @_ % 2 ) ) {
+        %options = @_;
+    }
+    elsif ( @_ == 1 ) {
+        $options{connect} = shift;
+    }
+    elsif (@_) {
+        croak "wrong paramater given for $class->standalone";
+    }
 
-	unless ($hostport) {
-		my $tmpdir = tempdir(CLEANUP => 1);
-		$hostport = "$tmpdir/~sock";
-	}
+    $options{bin} ||= 'transformator';
+    my $path =
+        $options{bin} =~ m{/}
+      ? $options{bin}
+      : ( Env::Path->PATH->Whence( $options{bin} ) )[0];
 
-	my $errstr;
+    unless ($path) {
+        croak "binary " . $options{bin} . " not found";
+    }
 
-	my $ok = 0;
+    unless ( $options{connect} ) {
+        my $tmpdir = tempdir( CLEANUP => 1 );
+        $options{connect} = "$tmpdir/~sock";
+    }
 
-	my $server = AnyEvent::Proc->new(
-		bin => $path,
-		args => [ $hostport ],
-		rtimeout => 10,
-		errstr => \$errstr,
-		on_rtimeout => sub { shift->kill; $ok = 0 },
-	);
+    my $errstr = '';
 
-	while (local $_ = $server->readline) {
-		AE::log debug => $_;
-		if (/server bound/) {
-			$ok = 1;
-			last;
-		}
-	}
+    my $cv = $options{cv} // AE::cv;
 
-	AE::log error => $errstr if $errstr;
+    my $server = AnyEvent::Proc->new(
+        bin         => $path,
+        args        => [ $options{connect} ],
+        rtimeout    => 10,
+        errstr      => \$errstr,
+        on_rtimeout => sub {
+            shift->fire_and_kill(
+                10,
+                sub {
+                    $cv->croak( 'timeout (' . $errstr . ')' );
+                }
+            );
+        },
+    );
 
-	$server->stop_rtimeout;
+    $server->readlines_cb(
+        sub {
+            if ( shift =~ m{server bound} ) {
+                $server->stop_rtimeout;
+                my $client = $class->new( $options{connect} );
+                $client->{_server} = $server;
+                $cv->send($client);
+            }
+        }
+    );
 
-	unless ($ok) {
-		$server->fire_and_kill(10);
-		AE::log fatal => 'standalone service not started';
-		return undef;
-	} else {
-		my $client = $class->new($hostport);
-		$client->{_server} = $server;
-		return $client;	
-	}
+    if ( $options{cb} or $options{cv} ) {
+        $cv->cb( $options{cb} ) if $options{cb};
+        return $cv;
+    }
+    else {
+        return $cv->recv;
+    }
 }
-
 
 sub cleanup {
-	my ($self) = @_;
-	if (exists $self->{_server}) {
-		my $server = delete $self->{_server};
-		$server->fire_and_kill(10);
-	} else {
-		AE::log note => "$self->cleanup called when no standalone server active";
-	}
+    my ($self) = @_;
+    if ( exists $self->{_server} ) {
+        my $server = delete $self->{_server};
+        $server->fire_and_kill(10);
+    }
+    else {
+        AE::log note =>
+          "$self->cleanup called when no standalone server active";
+    }
 
 }
 
+sub transform_cv {
+    my ( $self, %options ) = @_;
 
-sub transform_cv($%) {
-	my ($self, %options) = @_;
+    if ( $options{on_error} ) {
+        confess
+"on_error option is deprecated. the returned condvar will now croak on receive if there is an error.";
+    }
 
-	my $cv = AE::cv;
+    my $cv = $options{cv} || AE::cv;
 
-	my $err = sub {
-		$options{on_error}->(@_);
-		$cv->send(undef);
-	};
+    my $host = $self->{host};
+    my $port = $self->{port};
 
-	my $host = $self->{host};
-	my $port = $self->{port};
+    tcp_connect(
+        $host, $port,
+        sub {
+            return $cv->croak("Connect to $host:$port failed: $!") unless @_;
+            my ($fh) = @_;
+            my $AEH;
+            $AEH = AnyEvent::Handle->new(
+                fh       => $fh,
+                on_error => sub {
+                    my ( $handle, $fatal, $message ) = @_;
+                    $handle->destroy;
+                    $cv->croak("Socket error: $message");
+                },
+                on_eof => sub {
+                    $AEH->destroy;
+                },
+            );
+            $AEH->push_read(
+                cbor => sub {
+                    my $answer = $_[1];
+                    if ( defined $answer and ref $answer eq 'HASH' ) {
+                        if ( exists $answer->{error} ) {
+                            $cv->croak( "Service error: " . $answer->{error} );
+                        }
+                        elsif ( exists $answer->{result} ) {
+                            $cv->send( $answer->{result} );
+                        }
+                        else {
+                            $cv->croak(
+                                "Something is wrong: no result and no error");
+                        }
+                    }
+                    else {
+                        $cv->croak("No answer");
+                    }
+                }
+            );
+            $AEH->push_write( cbor =>
+                  [ $options{engine}, $options{input}, $options{data} || {} ] );
+        }
+    );
 
-	tcp_connect ($host, $port, sub {
-		return $err->("Connect to $host:$port failed: $!") unless @_;
-		my ($fh) = @_;
-		my $AEH;
-		$AEH = AnyEvent::Handle->new(
-			fh => $fh,
-			on_error => sub {
-				my ($handle, $fatal, $message) = @_;
-				$handle->destroy;
-				$err->("Socket error: $message");
-			},
-			on_eof => sub {
-				$AEH->destroy;
-			},
-		);
-		$AEH->push_read(cbor => sub {
-			my $answer = $_[1];
-			if (defined $answer and ref $answer eq 'HASH') {
-				if (exists $answer->{error}) {
-					$err->("Service error: ".$answer->{error});
-				} elsif (exists $answer->{result}) {
-					$cv->send($answer->{result});
-				} else {
-					$err->("Something is wrong: no result and no error");
-				}
-			} else {
-				$err->("No answer");
-			}
-		});
-		$AEH->push_write(cbor => [ $options{engine}, $options{input}, $options{data} || {} ]);
-	});
-	$cv;
+    $cv->cb( $options{cb} ) if $options{cb};
+
+    $cv;
 }
 
-
-sub transform($$$;$) {
-	my ($self, $engine, $input, $data) = @_;
-	my $error;
-	my $result = $self->transform_cv(
-		on_error => sub { $error = shift },
-		engine => $engine,
-		input => $input,
-		data => $data,
-	)->recv;
-	return $result if defined $result and not defined $error and not ref $result;
-	if (not defined $result and defined $error) {
-		AE::log error => $error;
-	} else {
-		AE::log error => "Something is wrong: $@";
-	}
+sub transform {
+    my ( $self, $engine, $input, $data ) = @_;
+    $self->transform_cv(
+        engine => $engine,
+        input  => $input,
+        data   => $data,
+    )->recv;
 }
 
+sub jade { shift->transform( jade => @_ ) }
 
-sub jade            ($$;$) { shift->transform(jade         => @_) }
+sub coffeescript { shift->transform( coffeescript => @_ ) }
 
+sub minify_html { shift->transform( minify_html => @_ ) }
 
-sub coffeescript    ($$;$) { shift->transform(coffeescript => @_) }
+sub minify_css { shift->transform( minify_css => @_ ) }
 
-
-sub minify_html     ($$;$) { shift->transform(minify_html  => @_) }
-
-
-sub minify_css      ($$;$) { shift->transform(minify_css   => @_) }
-
-
-sub minify_js       ($$;$) { shift->transform(minify_js    => @_) }
+sub minify_js { shift->transform( minify_js => @_ ) }
 
 1;
 
@@ -180,15 +205,13 @@ __END__
 
 =pod
 
-=encoding UTF-8
-
 =head1 NAME
 
 Net::NodeTransformator - interface to node transformator
 
 =head1 VERSION
 
-version 0.103
+version 0.104
 
 =head1 SYNOPSIS
 
@@ -225,13 +248,32 @@ Set the hostname/port or unix domain socket for connecting to transformator.
 	Net::NodeTransformator->new('localhost:12345');
 	Net::NodeTransformator->new('path/to/unix/domain/socket');	
 
-=head2 standalone([$hostport])
+=head2 standalone([$connect|%options])
 
-Starts a I<transformator> standalone server. If C<$hostport> is omitted, a temporary directory will be created and a unix domain socket will be placed in it.
+Starts a I<transformator> standalone server. If C<$connect> or C<$options{connect}> is omitted, a temporary directory will be created and a unix domain socket will be placed in it.
 
 Returns a ready-to-use L<Net::NodeTransformator> instance.
 
-	Net::NodeTransformator->standalone;
+	my $nnt = Net::NodeTransformator->standalone; # croaks on error
+
+Use C<$options{bin}> to either name the binary that could be found in I<$PATH> or name a direct path to the binary. Defaults to I<transformator>.
+
+Use C<$options{cb}> to set a callback handler, to avoid blocking.
+
+	Net::NodeTransformator->standalone(cb => sub {
+		my $nnt = shift->recv; # croaks on error
+	});
+
+Alternativly, use C<$options{cv}> to use the condvar directly
+
+	my $cv = Net::NodeTransformator->standalone(cv => 1);
+	my $nnt = $cv->recv; # croaks on error
+
+In both cases, a condvar is returned. An own condvar can also be used:
+
+	my $cv = AE::cv;
+	Net::NodeTransformator->standalone(cv => $cv);
+	$cv->recv; # croaks on error
 
 =head2 cleanup
 
@@ -251,15 +293,21 @@ Connects to transformator and waits for the result asynchronously by using a con
 
 =item C<data> (optional) Additional data to be send with. Currently only meaningful for I<jade> engine.
 
-=item C<on_error> A callback subroutine called in case of any error
+=item C<cv> (optional) An own AnyEvent::CondVar
+
+=item C<cb> (optional) A callback handler
+
+	$nnt->transform_cv(..., cb => sub {
+		my $result = shift->recv; # croaks on error
+	});
 
 =back
 
-This method returns a condition variable (L<AnyEvent>->condvar)
+This method returns a condition variable (L<AnyEvent>::CondVar)
 
 	my $cv = $nnt->transform_cv(...);
 
-The result will be pushed to the condvar, so C<$cv->recv> will return the result.
+The result will be pushed to the condvar, so C<< $cv->recv >> will return the result or croaks on error.
 
 =head2 transform($engine, $input, $data)
 
@@ -278,6 +326,15 @@ This list is incomplete. I will add more methods on request. All methods are hop
 =head2 minify_css($input)
 
 =head2 minify_js($input)
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website
+https://github.com/zurborg/libnet-nodetransformator-perl/issues
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
 
 =head1 AUTHOR
 
